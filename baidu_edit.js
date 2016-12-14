708208fd4e1651081aa8a122270cc093dfdb80d9
boyden <lee787359142@gmail.com>
2016-12-14 20:56:32 +0800

    Create baidu_edit.js

---
+++ baidu_edit.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Baidu
+// @version      0.0
+// @description  Baidu
+// @author       Boyden
+// @match        *://*/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+(function() {
+   var nav = document.getElementById("s_wrap");
+   nav.parentNode.removeChild(nav);
+
+   var title = document.getElementsByTagName("title");
+   title[0].innerHTML="百度";
+    
+    var search = document.getElementById("su");
+    search.value="搜索";
+})();
